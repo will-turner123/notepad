0b88b93acbda3e4bdedebf8742f29f342b9a1cf9
will-turner123 <willyrturner@gmail.com>
2022-11-10 17:16:04 -0500

    Full screen textarea

--- client/components/base.js
+++ client/components/base.js
@@ -3,9 +3,10 @@ import Header from "./header";
 
 export default function Base({ children }) {
     return (
-        <>
+        <div class="main-container">
+        
             <Header />
             {children}
-        </>
+        </div>
     )
 }
